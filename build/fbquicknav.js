e4fa3f9d7e29993e06bc6bf176bc624cc4f6ce7c
Francis Boudreau <francis@franboud.com>
2021-01-15 14:41:38 -0500

    Build JS.

--- build/fbquicknav.js
+++ build/fbquicknav.js
@@ -8,27 +8,28 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 /**
  * Quick Nav plugin.
- * Version 2.2
+ * Version 2.0
  *
  * Required:
- *    - LoDash _.debounce et _.throttle
+ *    - LoDash _.throttle
  *
  * Pour activer :
  *	  new FBQuickNav('.jsQuicknav');
  *    const quickNav = new FBQuickNav('.jsQuicknav');
+ *    const quickNav = new FBQuickNav('.jsQuicknav', { options });
  *
  * Options configurables :
- *    - section_class --> .jsQuickNav__section --> Sections added in the quick nav.
- *    - section_title --> data-quicknav-title --> Data attribute for the title of the section, displayed in the quick nav.
- *    - section_class_hide --> .jsQuickNav__hide --> When the quicknav reaches this section, hide it.
- *    - scroll_offset --> -100 (px) --> Offset of the scroll when we click on a quick nav link.
- *    - add_trigger --> false | true --> Add a button to open and close the quicknav.
- *    - trigger_after_items --> false | true --> Position of the trigger: before or after the items.
- *    - trigger_icon_open --> SVG --> SVG of the icon of the trigger to show the items.
- *    - trigger_icon_close --> SVG --> SVG of the icon of the trigger to hide the items.
+ *    - section_class --> Sections added in the quick nav.
+ *    - section_title --> Data attribute for the title of the section, displayed in the quick nav.
+ *    - section_class_hide --> When the quicknav reaches this section, hide it.
+ *    - scroll_offset --> Offset when scrolling to a section.
+ *    - add_trigger --> Add a button to open and close the quicknav.
+ *    - trigger_after_items --> Position of the trigger: before or after the items.
+ *    - trigger_icon_open --> SVG of the icon of the trigger to show the items.
+ *    - trigger_icon_close --> SVG of the icon of the trigger to hide the items.
  */
 var FBQuickNav = /*#__PURE__*/function () {
-  function FBQuickNav(selector) {
+  function FBQuickNav(selector, options) {
     _classCallCheck(this, FBQuickNav);
 
     this.selector = selector;
@@ -36,20 +37,22 @@ var FBQuickNav = /*#__PURE__*/function () {
 
     if (this.quicknavEl == null || typeof this.quicknavEl === "undefined") {
       return;
-    }
+    } // Default settings
+
 
     this.settings = {
       section_class: ".jsQuickNav__section",
       section_title: "data-quicknav-title",
-      section_class_hide: ".footer",
+      section_class_hide: ".jsQuickNav__hide",
       scroll_offset: -120,
       add_trigger: true,
       trigger_after_items: false,
-      // trigger_icon_open: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="M50 79.53L2.5 32.03l11.56-11.56L50 56.41l35.94-35.94L97.5 32.03z"/></svg>',
-      // trigger_icon_close: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="M1.004 89.605l88.6-88.6 9.397 9.397-88.6 88.6z"/><path d="M1.004 10.394L10.402.997l88.6 88.6-9.398 9.397z"/></svg>',
-      trigger_icon_open: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><path d="M24 28.82l.75-.65 8.4-7.27-1.49-1.72L24 25.81l-7.66-6.63-1.49 1.72 8.4 7.27.75.65z"/></svg>',
-      trigger_icon_close: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48"><path d="M34.53 34.4l-.09.1a1.11 1.11 0 01-1.82 0L24 25.93l-8.57 8.57a1.1 1.1 0 01-1.82 0l-.14-.1a1.11 1.11 0 010-1.82L22.09 24l-8.57-8.6a1.09 1.09 0 010-1.82l.09-.1a1.12 1.12 0 011.82 0L24 22.05l8.57-8.57a1.12 1.12 0 011.82 0l.09.1a1.11 1.11 0 010 1.82L25.92 24l8.61 8.61a1.12 1.12 0 010 1.79z"/></svg>'
-    };
+      trigger_icon_open: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="M50 79.53L2.5 32.03l11.56-11.56L50 56.41l35.94-35.94L97.5 32.03z"/></svg>',
+      trigger_icon_close: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><path d="M1.004 89.605l88.6-88.6 9.397 9.397-88.6 88.6z"/><path d="M1.004 10.394L10.402.997l88.6 88.6-9.398 9.397z"/></svg>'
+    }; // Merge options in the settings
+
+    this.settings = Object.assign(this.settings, options); // Init this plugin
+
     this.init();
   }
 
@@ -59,11 +62,11 @@ var FBQuickNav = /*#__PURE__*/function () {
       this.sectionsEl = document.querySelectorAll(this.settings.section_class + '[' + this.settings.section_title + ']'); // Seulement les sections qui ont un titre defined.
 
       this.build_html();
-      this.click_on_quicknav_event();
-      this.toggle_quicknav_classes();
-      this.click_on_trigger_event();
+      this.click_items_events();
+      this.scroll_activate_items();
+      this.open_close_trigger();
       this.close_on_scroll();
-      this.toggle_hide_quicknav(); // Tout est fait, init quicknav
+      this.hide_quicknav(); // Tout est fait, init quicknav
 
       this.quicknavEl.classList.add("isInit");
     }
@@ -168,14 +171,89 @@ var FBQuickNav = /*#__PURE__*/function () {
       this.set_trigger_label(this.sectionsEl[0]);
     }
     /**
+     * Click on the quicknav link.
+     * When there's a click, the page scrolls to the chosen section.
+     */
+
+  }, {
+    key: "click_items_events",
+    value: function click_items_events() {
+      var _this2 = this;
+
+      this.quicknavEl.addEventListener("click", function (evt) {
+        // On cible seulement les liens <a>
+        // ATTENTION! closest bubbles up jusqu'au document.
+        var linkEl = evt.target.closest("a.quickNav__item");
+
+        if (!linkEl) {
+          return;
+        }
+
+        ;
+        evt.preventDefault();
+        var section_index = linkEl.getAttribute("data-quicknav-index");
+
+        var sectionEl = _this2.find_section_by_index(section_index);
+
+        if (sectionEl != null) {
+          _this2.go_to_section(sectionEl);
+        }
+      });
+    }
+    /**
+     * Gerer l'activation automatique des classes "active" sur les items.
+     * Quand le user scroll, on indique sur quelle section on se trouve.
+     */
+
+  }, {
+    key: "scroll_activate_items",
+    value: function scroll_activate_items() {
+      var _this3 = this;
+
+      this.sectionsEl.forEach(function (sectionEl) {
+        var sectionIndex = sectionEl.getAttribute("data-quicknav-index"),
+            linkEl = _this3.find_link_by_index(sectionIndex);
+
+        ScrollTrigger.create({
+          trigger: sectionEl,
+          // Describes a place on the trigger and a place on the scroller 
+          // that must meet in order to start the ScrollTrigger.
+          // Example:
+          // "top center" = "when the top of the trigger hits the center of the scroller"
+          // "bottom 80%" = "when the bottom of the trigger hits 80% down from the top of the viewport" 
+          start: "top center",
+          end: "bottom center",
+          // Gestion custom des events
+          onEnter: function onEnter() {
+            linkEl.classList.add("active");
+
+            _this3.set_trigger_label(sectionEl);
+          },
+          onLeave: function onLeave() {
+            linkEl.classList.remove("active");
+          },
+          onEnterBack: function onEnterBack() {
+            linkEl.classList.add("active");
+
+            _this3.set_trigger_label(sectionEl);
+          },
+          onLeaveBack: function onLeaveBack() {
+            linkEl.classList.remove("active");
+          } // markers: true,
+          // id: "section " + sectionIndex,
+
+        });
+      });
+    }
+    /**
      * Click on the trigger button.
      * Open and close the quicknav.
      */
 
   }, {
-    key: "click_on_trigger_event",
-    value: function click_on_trigger_event() {
-      var _this2 = this;
+    key: "open_close_trigger",
+    value: function open_close_trigger() {
+      var _this4 = this;
 
       if (this.settings.add_trigger !== true) {
         return;
@@ -185,11 +263,64 @@ var FBQuickNav = /*#__PURE__*/function () {
       triggerEl.addEventListener("click", function (evt) {
         evt.preventDefault();
 
-        if (_this2.quicknavEl.classList.contains("open")) {
-          _this2.close_quicknav();
+        if (_this4.quicknavEl.classList.contains("open")) {
+          _this4.close_quicknav();
         } else {
-          _this2.open_quicknav();
+          _this4.open_quicknav();
         }
+      });
+    }
+    /**
+     * Close the quicknav on scroll
+     */
+
+  }, {
+    key: "close_on_scroll",
+    value: function close_on_scroll() {
+      var _this5 = this;
+
+      if (this.settings.add_trigger !== true) {
+        return;
+      }
+
+      window.addEventListener('scroll', _.throttle(function () {
+        if (_this5.quicknavEl.classList.contains("open")) {
+          _this5.close_quicknav();
+        }
+      }, 300));
+    }
+    /**
+     * Hide the quicknav
+     * When the window reach a section, hide the quicknav.
+     * Usually, this section is the footer.
+     */
+
+  }, {
+    key: "hide_quicknav",
+    value: function hide_quicknav() {
+      var hideHereEl = document.querySelector(this.settings.section_class_hide);
+
+      if (!hideHereEl) {
+        return;
+      }
+
+      ScrollTrigger.create({
+        trigger: hideHereEl,
+        toggleClass: {
+          targets: this.quicknavEl,
+          className: "quickNav--hide"
+        },
+        // Describes a place on the trigger and a place on the scroller 
+        // that must meet in order to start the ScrollTrigger.
+        // Example:
+        // "top center" = "when the top of the trigger hits the center of the scroller"
+        // "bottom 80%" = "when the bottom of the trigger hits 80% down from the top of the viewport" 
+        start: "top 25%",
+        // Ce code permet de ne pas avoir de fin au ScrollTrigger
+        endTrigger: "html",
+        end: "bottom top" // markers: true,
+        // id: "hide quicknav",
+
       });
     }
     /**
@@ -209,25 +340,6 @@ var FBQuickNav = /*#__PURE__*/function () {
       triggerLabelEl.textContent = sectionName;
     }
     /**
-     * Close the quicknav on scroll
-     */
-
-  }, {
-    key: "close_on_scroll",
-    value: function close_on_scroll() {
-      var _this3 = this;
-
-      if (this.settings.add_trigger !== true) {
-        return;
-      }
-
-      window.addEventListener('scroll', _.throttle(function () {
-        if (_this3.quicknavEl.classList.contains("open")) {
-          _this3.close_quicknav();
-        }
-      }, 300));
-    }
-    /**
      * Open the quicknav
      */
 
@@ -244,36 +356,6 @@ var FBQuickNav = /*#__PURE__*/function () {
     key: "close_quicknav",
     value: function close_quicknav() {
       this.quicknavEl.classList.remove('open');
-    }
-    /**
-     * Click on the quicknav link.
-     * When there's a click, the page scrolls to the chosen section.
-     */
-
-  }, {
-    key: "click_on_quicknav_event",
-    value: function click_on_quicknav_event() {
-      var _this4 = this;
-
-      this.quicknavEl.addEventListener("click", function (evt) {
-        // On cible seulement les liens <a>
-        // ATTENTION! closest bubbles up jusqu'au document.
-        var linkEl = evt.target.closest("a.quickNav__item");
-
-        if (!linkEl) {
-          return;
-        }
-
-        ;
-        evt.preventDefault();
-        var section_index = linkEl.getAttribute("data-quicknav-index");
-
-        var sectionEl = _this4.find_section_by_index(section_index);
-
-        if (sectionEl != null) {
-          _this4.go_to_section(sectionEl);
-        }
-      });
     }
     /**
      * Trouver une section par index de section (data-quicknav-index).
@@ -327,85 +409,6 @@ var FBQuickNav = /*#__PURE__*/function () {
       // 	block: 'center', // Defines vertical alignment. default: start
       // 	inline: 'nearest' // Defines horizontal alignment. default: nearest
       // });
-    }
-    /**
-     * Gerer l'activation automatique des classes "active" sur les items.
-     * Quand le user scroll, on indique sur quelle section on se trouve.
-     */
-
-  }, {
-    key: "toggle_quicknav_classes",
-    value: function toggle_quicknav_classes() {
-      var _this5 = this;
-
-      this.sectionsEl.forEach(function (sectionEl) {
-        var sectionIndex = sectionEl.getAttribute("data-quicknav-index"),
-            linkEl = _this5.find_link_by_index(sectionIndex);
-
-        ScrollTrigger.create({
-          trigger: sectionEl,
-          // Describes a place on the trigger and a place on the scroller 
-          // that must meet in order to start the ScrollTrigger.
-          // Example:
-          // "top center" = "when the top of the trigger hits the center of the scroller"
-          // "bottom 80%" = "when the bottom of the trigger hits 80% down from the top of the viewport" 
-          start: "top center",
-          end: "bottom center",
-          // Gestion custom des events
-          onEnter: function onEnter() {
-            linkEl.classList.add("active");
-
-            _this5.set_trigger_label(sectionEl);
-          },
-          onLeave: function onLeave() {
-            linkEl.classList.remove("active");
-          },
-          onEnterBack: function onEnterBack() {
-            linkEl.classList.add("active");
-
-            _this5.set_trigger_label(sectionEl);
-          },
-          onLeaveBack: function onLeaveBack() {
-            linkEl.classList.remove("active");
-          } // markers: true,
-          // id: "section " + sectionIndex,
-
-        });
-      });
-    }
-    /**
-     * Hide the quicknav
-     * When the window reach a section, hide the quicknav.
-     * Usually, this section is the footer.
-     */
-
-  }, {
-    key: "toggle_hide_quicknav",
-    value: function toggle_hide_quicknav() {
-      var hideHereEl = document.querySelector(this.settings.section_class_hide);
-
-      if (!hideHereEl) {
-        return;
-      }
-
-      ScrollTrigger.create({
-        trigger: hideHereEl,
-        toggleClass: {
-          targets: this.quicknavEl,
-          className: "quickNav--hide"
-        },
-        // Describes a place on the trigger and a place on the scroller 
-        // that must meet in order to start the ScrollTrigger.
-        // Example:
-        // "top center" = "when the top of the trigger hits the center of the scroller"
-        // "bottom 80%" = "when the bottom of the trigger hits 80% down from the top of the viewport" 
-        start: "top 25%",
-        // Ce code permet de ne pas avoir de fin au ScrollTrigger
-        endTrigger: "html",
-        end: "bottom top" // markers: true,
-        // id: "hide quicknav",
-
-      });
     }
   }]);
 
